edb8a4fceb5178a809023aa6729d83c040394f29
Matthew Hart <mattevanhart@gmail.com>
2021-04-21 23:27:09 -0400

    header/title

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,11 +13,12 @@ const Header = () => {
 
 	return (
 		<Wrapper>
+			<h1>Cirqit</h1>
 			{!displayName && <button onClick={signInWithGoogle}>Sign In</button>}
 			{displayName && !enterDisplayName && (
 				<>
 					<span>
-						<h1>Hi, {displayName}</h1>
+						<h2>Hi, {displayName}</h2>
 					</span>
 					<SignOut onClick={handleSignOut}>Sign Out</SignOut>
 				</>
@@ -30,20 +31,25 @@ const Wrapper = styled.div`
 	grid-area: header;
 	display: flex;
 	align-items: center;
-	justify-content: flex-end;
+	justify-content: space-between;
+	h1,
+	h2 {
+		user-select: none;
+	}
+	h1 {
+		padding: 0 15px;
+		font-size: 22px;
+	}
 	span {
 		height: 101%;
 		width: 150px;
-		margin: 0 15px;
+		padding: 0 15px;
 		display: flex;
 		align-items: center;
 		justify-content: flex-end;
 		&:hover + button {
 			z-index: 10;
 		}
-	}
-	h1 {
-		user-select: none;
 	}
 	button {
 		width: 120px;
